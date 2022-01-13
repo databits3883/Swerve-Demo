9463f2cc72bba81c2829c573475935c11b9d663e
alex-vanasse <49699649+alex-vanasse@users.noreply.github.com>
2022-01-13 17:45:38 -0600

    initial commit to upload working project

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.Set;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain m_drivetrain = new Drivetrain();
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final Joystick m_stick = new Joystick(1);
+
+  private final Command m_manualDrive = new Command() {
+    @Override
+    public void execute(){
+      ChassisSpeeds speeds = new ChassisSpeeds(-2*m_stick.getRawAxis(1),2*m_stick.getRawAxis(0),3*m_stick.getRawAxis(3));
+      //m_drivetrain.setSpeed(speeds);
+      m_drivetrain.setSpeedGyroRelative(speeds);
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+      // TODO Auto-generated method stub
+      return Set.of(m_drivetrain);
+    }
+  }; 
+
+  private final Trigger setButton = new JoystickButton(m_stick, 2).whileActiveOnce(new InstantCommand(m_drivetrain::resetGyro,m_drivetrain));
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    m_drivetrain.setDefaultCommand(m_manualDrive);
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,116 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.databits3883.databitslib.sparkmax.SparkMaxPIDController;
+import com.databits3883.databitslib.swerveControl.SwerveDrive;
+import com.databits3883.databitslib.swerveControl.SwerveModule;
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxLimitSwitch.Type;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.*;
+
+import java.util.List;
+
+public class Drivetrain extends SubsystemBase {
+  SwerveDrive m_drive;
+
+  AHRS m_gyro;
+
+  SparkMaxLimitSwitch[] triggers = new SparkMaxLimitSwitch[4];
+  SwerveModule[] modules = new SwerveModule[4];
+  CANSparkMax[] rotationMotors = new CANSparkMax[4];
+
+  static final double[] switchLocations = {-4.122, -2.54,-1.03,0.598};
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    List<Pair<SwerveModule,Translation2d>> modulesWithLoc = List.of(
+      Pair.of(newModule(1,2,"front right"),new Translation2d(driveTrackLength/2,driveTrackWidth/2)),
+      Pair.of(newModule(3,4,"rear right"),new Translation2d(-driveTrackLength/2,driveTrackWidth/2)),
+      Pair.of(newModule(5,6,"rear left"),new Translation2d(-driveTrackLength/2,-driveTrackWidth/2)),
+      Pair.of(newModule(7,8,"front left"),new Translation2d(driveTrackLength/2,-driveTrackWidth/2))
+      
+    );
+    m_drive = new SwerveDrive(modulesWithLoc);
+
+    m_gyro = new AHRS(I2C.Port.kMXP);
+    addChild("Gyro", m_gyro);
+    SendableRegistry.addLW(this, "drivetrain");
+  }
+
+  SwerveModule newModule(int velChannel, int angleChannel, String name){
+    CANSparkMax rot = new CANSparkMax(angleChannel,MotorType.kBrushless);
+    rot.setInverted(true);
+    rot.getAlternateEncoder(4096).setInverted(true);
+    CANSparkMax vel = new CANSparkMax(velChannel,MotorType.kBrushless);
+    SparkMaxPIDController rotC = SparkMaxPIDController.withAlternateEncoder(rot, ControlType.kPosition, 4096);
+    rotC.setP(0.7);
+    rotC.setI(0);
+    rotC.setD(0);
+    rotC.setSetpoint(0);
+    rotC.setSetpoint(rotC.getSignal());
+    SparkMaxPIDController velC = SparkMaxPIDController.withDefaultEncoder(vel, ControlType.kVelocity);
+    velC.setP(0.25);
+    velC.setI(0);
+    velC.setD(1.0);
+    velC.setFF(0.27);
+    SwerveModule module = new SwerveModule(velC,rotC);
+    addChild(name+" rotation", rotC);
+    addChild(name+" velocity", velC);
+    module.setAngleConversionFactor(rotateWheelGear);
+    module.setVelocityConversionFactor(velocityWheelGear * wheelCircumfrence * (1.0/60.0));
+ 
+    triggers[velChannel/2] = vel.getReverseLimitSwitch(Type.kNormallyOpen);
+    modules[velChannel/2] = module;
+
+    return module;
+  }
+
+  public void setSpeed(ChassisSpeeds speeds){
+    m_drive.setChassisSpeed(speeds);
+  }
+
+  public void setSpeedGyroRelative(ChassisSpeeds speeds){
+    m_drive.setChassisSpeed(ChassisSpeeds.fromFieldRelativeSpeeds(
+      speeds.vxMetersPerSecond,
+      speeds.vyMetersPerSecond,
+      speeds.omegaRadiansPerSecond, 
+      Rotation2d.fromDegrees(m_gyro.getAngle())
+    ));
+  }
+
+  public void resetGyro(){
+    m_gyro.reset();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    if(RobotState.isDisabled()){
+      for(int i=0;i<4;i++){
+        if(triggers[i].isPressed()){
+          //double calibrationPoint = Math.PI/4.0 +(i*Math.PI/2);
+          //modules[i].setWheelAngle(new Rotation2d(calibrationPoint));
+          modules[i].setWheelAngle(new Rotation2d(switchLocations[i]));
+        }
+      }
+    }
+  }
+}
